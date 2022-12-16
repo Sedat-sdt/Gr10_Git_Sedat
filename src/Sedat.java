d2f9ae37cc38c5014c7a22060a63a46bc589e107
Sedat Maden <madensedat05@gmail.com>
2022-12-16 19:09:34 +0300

    my last additional commit 1

--- src/Sedat.java
+++ src/Sedat.java
@@ -1,9 +1,11 @@
-public class Ziya {
+public class Sedat {
     public static void main(String[] args) {
         System.out.println("my first change");
         System.out.println("my second change");
         System.out.println("my last change");
         System.out.println("my last additional change");
+        System.out.println("my last additional change 1");
+
 
     }
 }
